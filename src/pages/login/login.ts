7cd40d61015926780d49fb7a60157865f94d69db
Tobias Larsson <tobias@raqt.com>
2017-07-26 18:17:28 +0200

    Icons and splash

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ToastController, ViewController } from 'ionic-angular';
+import { NavParams, ToastController, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the LoginPage page.
