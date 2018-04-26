18b837abc2e377f90e52fa10bc4b38086d2a0ab6
Tobias Larsson <toblar@raysearchlabs.com>
2018-04-26 11:16:55 +0200

    Ion slide change load banks and accounts
    New arrows red and green
    New pipes

---
+++ src/pipes/format-currency/format-currency.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as numeral from 'numeral';
+/**
+ * Generated class for the FormatCurrencyPipe pipe.
+ *
+ * See https://angular.io/api/core/Pipe for more info on Angular Pipes.
+ */
+@Pipe({
+  name: 'formatCurrency',
+})
+export class FormatCurrencyPipe implements PipeTransform {
+  /**
+   * Takes a value and makes it lowercase.
+   */
+  transform(value: string, args: string) {
+
+    let num = numeral(value).format('0,0.00');
+    console.log('Currency ' + num)
+    return num;
+  }
+}
--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CurrencyPipe } from './currency/currency';
 import { MomentFormatPipe } from './moment-format/moment-format';
+import { MomentPipe } from './moment.pipe';
+import { FormatCurrencyPipe } from './format-currency/format-currency';
+import { CurrencySymbolPipe } from './currency-symbol/currency-symbol';
 @NgModule({
 	declarations: [CurrencyPipe,
-    MomentFormatPipe],
+		MomentFormatPipe, MomentPipe,
+    FormatCurrencyPipe,
+    CurrencySymbolPipe],
 	imports: [],
 	exports: [CurrencyPipe,
-    MomentFormatPipe]
+		MomentFormatPipe, MomentPipe,
+    FormatCurrencyPipe,
+    CurrencySymbolPipe]
 })
-export class PipesModule {}
+export class PipesModule { }
